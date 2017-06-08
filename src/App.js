feb5e76dc287d4d57c129a9d77a9fa578ccb2fc1
sabativi <victor.sabatier@gmail.com>
2017-06-08 18:37:45 +0200

    Network Error

--- src/App.js
+++ src/App.js
@@ -3,34 +3,20 @@ import { gql, graphql } from 'react-apollo';
 
 class App extends Component {
   render() {
-    const { data: { loading, people } } = this.props;
+    const { data: { refetch, loading, hero, error } } = this.props;
+    if(loading) {
+      return (<p>Loading…</p>);
+    }
     return (
       <main>
         <header>
           <h1>Apollo Client Error Template</h1>
-          <p>
-            This is a template that you can use to demonstrate an error in Apollo Client.
-            Edit the source code and watch your browser window reload with the changes.
-          </p>
-          <p>
-            The code which renders this component lives in <code>./src/App.js</code>.
-          </p>
-          <p>
-            The GraphQL schema is in <code>./src/graphql/schema</code>.
-            Currently the schema just serves a list of people with names and ids.
-          </p>
+          <p>{error && error.message}</p>
+          <h3>{!error && hero.name}</h3>
+          <button onClick={() => refetch()}>
+            Refetch
+          </button>
         </header>
-        {loading ? (
-          <p>Loading…</p>
-        ) : (
-          <ul>
-            {people.map(person => (
-              <li key={person.id}>
-                {person.name}
-              </li>
-            ))}
-          </ul>
-        )}
       </main>
     );
   }
@@ -38,9 +24,8 @@ class App extends Component {
 
 export default graphql(
   gql`{
-    people {
-      id
+    hero {
       name
     }
-  }`,
+}`,
 )(App)
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createNetworkInterface } from 'apollo-client';
 import { ApolloClient, ApolloProvider } from 'react-apollo';
-import { networkInterface } from './graphql/networkInterface';
 import App from './App';
+
+const networkInterface = createNetworkInterface({
+  uri: "https://mpjk0plp9.lp.gql.zone/graphql",
+});
 
 const client = new ApolloClient({ networkInterface });
 
